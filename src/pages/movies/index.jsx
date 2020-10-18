29174d228e37f3db3e785b3b788da470026a3769
unknown <henriquehps1997@gmail.com>
2020-10-18 16:29:41 -0300

    ajuste nas telas de implementacao de slides

--- src/pages/movies/index.jsx
+++ src/pages/movies/index.jsx
@@ -1,16 +1,53 @@
 import React from 'react';
 
-import Slide from '../../components/slide/index';
+import Slider from '../../components/slider';
 
 import { Carousel } from 'react-responsive-carousel';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-function Index(){
+const base = "file:///C:/Apache24/htdocs/themovie-frontend/src/pages/movies/images";
 
-    // const onChange = (value) => {
-    //     console.log(value)
-    // }
+const movies = [
+    {
+      id: 1,
+      image: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      imageBg: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      title: '1983'
+    },
+    {
+      id: 2,
+      image: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      imageBg: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      title: 'Russian doll'
+    },
+    {
+      id: 3,
+      image: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      imageBg: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      title: 'The rain',
+    },
+    {
+      id: 4,
+      image: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      imageBg: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      title: 'Sex education'
+    },
+    {
+      id: 5,
+      image: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      imageBg: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      title: 'Elite'
+    },
+    {
+      id: 6,
+      image: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      imageBg: 'https://occ-0-4356-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABffpRGxaTHHjcCcgchWPQdBIPrcwnxbeXWtfV4YcxPK_Ff1RJ5rZVu_tCEii8KrqCnAHNfRSct13LvxAmOd57HeO6H1z.webp?r=732',
+      title: 'Black mirror'
+    }
+];    
+
+function Index(){
 
     return(
         <>
@@ -35,19 +72,11 @@ function Index(){
                     <div className={`col-md-12`}>
                         <h3 className={`text-white`}>Filmes populares</h3>
                     </div>
-                    <Slide />
-                </div>
-                <div className={`row justify-content-center mt-5`}>
-                    <div className={`col-md-12`}>
-                        <h3 className={`text-white`}>Drama</h3>
-                    </div>
-                    <Slide />
-                </div>
-                <div className={`row justify-content-center mt-5`}>
-                    <div className={`col-md-12`}>
-                        <h3 className={`text-white`}>Ação</h3>
-                    </div>
-                    <Slide />
+                    <Slider>
+                        {movies.map(movie => (
+                            <Slider.Item movie={movie} key={movie.id}>item1</Slider.Item>
+                        ))}
+                    </Slider>
                 </div>
             </div>
         </>
